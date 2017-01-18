1f7f070f8b4bae8befb78669c4a0abc565d0b624
Vladimir Logachev <doit@keemail.me>
2017-01-18 20:16:10 +0700

    Updated data

--- src/js_src/initial-data.js
+++ src/js_src/initial-data.js
@@ -1,94 +1,121 @@
 var initialRecipeList = [
 {
-	name: 'Zero Recipe',
+	name: 'Lemon drizzle cake',
 	ingredients: [
 	{
-		name: 'Ingredient 0.0',
-		amount: 250,
+		name: 'unsalted butter, softened',
+		amount: 225,
+		measure: 'g'
+	},
+	{
+		name: 'caster sugar',
+		amount: 225,
+		measure: 'g'
+	},
+	{
+		name: 'egg',
+		amount: 4,
+		measure: 'pcs'
+	},
+	{
+		name: 'finely grated lemon zest',
+		amount: 1,
+		measure: 'pcs'
+	},
+	{
+		name: 'lemon juice for the drizzle topping',
+		amount: 50,
 		measure: 'ml'
 	},
 	{
-		name: 'Ingredient 0.1',
-		amount: 4,
+		name: 'caster sugar for the drizzle topping',
+		amount: 70,
 		measure: 'g'
 	},
-	{
-		name: 'Ingredient 0.2',
-		amount: 5,
-		measure: 'g'
-	}
 
 	],
-	instructions: 'mix it up and wait 3 minutes',
-	date: 'today'
+	instructions: 'Heat oven to 180C/fan 160C/gas 4. Beat together 225g softened unsalted butter and 225g caster sugar until pale and creamy, then add 4 eggs, one at a time, slowly mixing through. Sift in 225g flour, then add the finely grated zest of 1 lemon and mix until well combined. Line a loaf tin (8 x 21cm) with greaseproof paper, then spoon in the mixture and level the top with a spoon.\nBake for 45-50 mins until a thin skewer inserted into the centre of the cake comes out clean. While the cake is cooling in its tin, mix together the juice of 1 1/2 lemons and 85g caster sugar to make the drizzle. Prick the warm cake all over with a skewer or fork, then pour over the drizzle – the juice will sink in and the sugar will form a lovely, crisp topping. Leave in the tin until completely cool, then remove and serve. Will keep in an airtight container for 3-4 days, or freeze for up to 1 month.',
+	date: '???'
 },
 {
-	name: 'One Recipe',
+	name: 'Even better lemon cake',
 	ingredients: [
 	{
-		name: 'Ingredient 1.0',
-		amount: 250,
+		name: 'unsalted butter, softened',
+		amount: 225,
+		measure: 'g'
+	},
+	{
+		name: 'caster sugar',
+		amount: 225,
+		measure: 'g'
+	},
+	{
+		name: 'egg',
+		amount: 4,
+		measure: 'pcs'
+	},
+	{
+		name: 'finely grated lemon zest',
+		amount: 1,
+		measure: 'pcs'
+	},
+	{
+		name: 'lemon juice for the drizzle topping',
+		amount: 50,
 		measure: 'ml'
 	},
 	{
-		name: 'Ingredient 1.1',
-		amount: 4,
+		name: 'caster sugar for the drizzle topping',
+		amount: 70,
 		measure: 'g'
 	},
-	{
-		name: 'Ingredient 1.2',
-		amount: 5,
-		measure: 'g'
-	}
 
 	],
-	instructions: 'mix it up and wait 3 minutes',
-	date: 'today'
+	instructions: 'Heat oven to 180C/fan 160C/gas 4. Beat together 225g softened unsalted butter and 225g caster sugar until pale and creamy, then add 4 eggs, one at a time, slowly mixing through. Sift in 225g flour, then add the finely grated zest of 1 lemon and mix until well combined. Line a loaf tin (8 x 21cm) with greaseproof paper, then spoon in the mixture and level the top with a spoon.\nBake for 45-50 mins until a thin skewer inserted into the centre of the cake comes out clean. While the cake is cooling in its tin, mix together the juice of 1 1/2 lemons and 85g caster sugar to make the drizzle. Prick the warm cake all over with a skewer or fork, then pour over the drizzle – the juice will sink in and the sugar will form a lovely, crisp topping. Leave in the tin until completely cool, then remove and serve. Will keep in an airtight container for 3-4 days, or freeze for up to 1 month.',
+	date: '???'
 },
 {
-	name: 'Two Recipe',
+	name: 'Lemon drizzle cake with vodka',
 	ingredients: [
 	{
-		name: 'Ingredient 2.0',
-		amount: 250,
+		name: 'unsalted butter, softened',
+		amount: 225,
+		measure: 'g'
+	},
+	{
+		name: 'vodka',
+		amount: 100,
 		measure: 'ml'
 	},
 	{
-		name: 'Ingredient 2.1',
-		amount: 4,
+		name: 'caster sugar',
+		amount: 225,
 		measure: 'g'
 	},
 	{
-		name: 'Ingredient 2.2',
-		amount: 5,
-		measure: 'g'
-	}
-
-	],
-	instructions: 'mix it up and wait 6 minutes',
-	date: 'yesterday'
-},
-{
-	name: 'Three Recipe',
-	ingredients: [
+		name: 'egg',
+		amount: 4,
+		measure: 'pcs'
+	},
 	{
-		name: 'Ingredient 3.0',
-		amount: 250,
+		name: 'finely grated lemon zest',
+		amount: 1,
+		measure: 'pcs'
+	},
+	{
+		name: 'lemon juice for the drizzle topping',
+		amount: 50,
 		measure: 'ml'
 	},
 	{
-		name: 'Ingredient 3.1',
-		amount: 4,
+		name: 'caster sugar for the drizzle topping',
+		amount: 70,
 		measure: 'g'
 	},
-	{
-		name: 'Ingredient 3.2',
-		amount: 5,
-		measure: 'g'
-	}
 
 	],
-	instructions: 'mix it up and wait 6 minutes',
-	date: 'yesterday'
+	instructions: 'Heat oven to 180C/fan 160C/gas 4. Beat together 225g softened unsalted butter and 225g caster sugar until pale and creamy, then add 4 eggs, one at a time, slowly mixing through. Sift in 225g flour, then add the finely grated zest of 1 lemon and mix until well combined. Line a loaf tin (8 x 21cm) with greaseproof paper, then spoon in the mixture and level the top with a spoon.\nBake for 45-50 mins until a thin skewer inserted into the centre of the cake comes out clean. While the cake is cooling in its tin, mix together the juice of 1 1/2 lemons and 85g caster sugar to make the drizzle. Prick the warm cake all over with a skewer or fork, then pour over the drizzle – the juice will sink in and the sugar will form a lovely, crisp topping. Leave in the tin until completely cool, then remove and serve. Will keep in an airtight container for 3-4 days, or freeze for up to 1 month.\nHope you know what to do with vodka.',
+	date: '???'
 }
 ];
